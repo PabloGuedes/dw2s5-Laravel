3c194cd79e4c517707889e21dcd73c7de7621152
Pablo Guedes Bacca <guedespablobacca@gmail.com>
2024-07-03 14:57:04 -0300

    Até a aula #10

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,10 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\EventController;
+
+Route::get('/', [EventController::class, 'index']);
+Route::get('/events/create', [EventController::class, 'create']);
 
 Route::get('/', function () {
     return view('welcome');
