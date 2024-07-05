c6c06cef5490bc6a1ea054be3090155452ec9f73
Pablo Guedes Bacca <guedespablobacca@gmail.com>
2024-07-05 08:03:37 -0300

    Até a aula #15

--- app/Http/Controllers/EventController.php
+++ app/Http/Controllers/EventController.php
@@ -8,13 +8,26 @@ use App\Models\Event;
 
 class EventController extends Controller
 {
-    public function index(){
+    public function index() {
         $events = Event::all();
 
         return view('welcome', ['events' => $events]);
     }
 
-    public function create(){
+    public function create() {
         return view('events.create');
+    }
+
+    public function store(request $request) {
+        $event = new Event;
+
+        $event->title = $request->title;
+        $event->city = $request->city;
+        $event->private = $request->private;
+        $event->description = $request->description;
+
+        $event->save();
+
+        return redirect('/');
     }
 }
