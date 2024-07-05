835eed946f98d2146f7cbec1c8d187e39ae9e86d
Pablo Guedes Bacca <guedespablobacca@gmail.com>
2024-07-05 11:23:24 -0300

    Aula #18

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -56,7 +56,8 @@
         <p>HDC Events &copy; 2024</p>
     </footer>
 
-    <script src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.esm.js"></script>
+    <script nomodule src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.js"></script>
+    <script type="module" src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.esm.js"></script>
 </body>
 
 </html>
