bc4fddaf296e7a6efb3b856cb3838e2e6bacf690
Pablo Guedes Bacca <guedespablobacca@gmail.com>
2024-07-05 12:10:10 -0300

    Aula #20

--- resources/views/events/create.blade.php
+++ resources/views/events/create.blade.php
@@ -6,24 +6,24 @@
 
 <div id="event-create-container" class="col-md-6 offset-md-3">
   <h1>Crie o seu evento</h1>
-  <br>
   <form action="/events" method="POST" enctype="multipart/form-data">
     @csrf
     <div class="form-group">
       <label for="image">Imagem do Evento:</label>
       <input type="file" id="image" name="image" class="from-control-file">
     </div>
-    <br>
     <div class="form-group">
       <label for="title">Evento:</label>
       <input type="text" class="form-control" id="title" name="title" placeholder="Nome do evento">
     </div>
-    <br>
+    <div class="form-group">
+      <label for="date">Data do evento:</label>
+      <input type="date" class="form-control" id="date" name="date">
+    </div>
     <div class="form-group">
       <label for="title">Cidade:</label>
       <input type="text" class="form-control" id="city" name="city" placeholder="Local do evento">
     </div>
-    <br>
     <div class="form-group">
       <label for="title">O evento é privado?</label>
       <select name="private" id="private" class="form-control">
@@ -31,12 +31,10 @@
         <option value="1">Sim</option>
       </select>
     </div>
-    <br>
     <div class="form-group">
       <label for="title">Descrição:</label>
       <textarea name="description" id="description" class="form-control" placeholder="O que vai acontecer no evento?"></textarea>
     </div>
-    <br>
     <div class="form-group">
       <label for="title">Adicione itens de infraestrutura:</label>
       <div class="form-group">	
@@ -55,7 +53,6 @@
         <input type="checkbox" name="items[]" value="Brindes"> Brindes
       </div>
     </div>
-    <br>
     <input type="submit" class="btn btn-primary" value="Criar Evento">
   </form>
 </div>
